02f21d7db085fd44963ef1f90b8478b16fb18273
Alex Elias <aelias36@yahoo.com>
2024-08-15 12:44:04 -0400

    Add rotation part to helix toolpath

--- +example_toolpath/helix.m
+++ +example_toolpath/helix.m
@@ -1,4 +1,4 @@
-function p_path = helix()
+function [p_path, R_path] = helix()
 R = 0.1;
 H = 0.3;
 TURNS = 5;
@@ -12,4 +12,16 @@ Y = R*sin(2*pi*TURNS*lambda);
 Z = H*lambda;
 
 p_path = [X;Y;Z];
+
+
+ex = [1;0;0];
+ey = [0;1;0];
+ez = [0;0;1];
+R_path = NaN(3, 3, N);
+R_0 = rot(ey, pi+pi/6);
+
+for i = 1:N
+    R_path(:,:,i) = rot(ez, 2*pi*TURNS*lambda(i))*R_0;
+end
+
 end
--- unit_tests/test_helix.m
+++ unit_tests/test_helix.m
@@ -1,4 +1,25 @@
+%% Plot just position part
 p_path = example_toolpath.helix();
 
 plot3(p_path(1,:), p_path(2,:), p_path(3,:), '-x');
 axis equal
+
+%% Plot position and rotation
+
+[p_path, R_path] = example_toolpath.helix();
+
+diagrams.setup(); hold on
+diagrams.utils.plot3_mat(p_path);
+
+UNIT_SIZE = 0.05;
+
+for i = 1:30:length(p_path)
+    diagrams.arrow(p_path(:,i), p_path(:,i)+UNIT_SIZE*R_path(:,1,i), 'color', diagrams.colors.red);
+    diagrams.arrow(p_path(:,i), p_path(:,i)+UNIT_SIZE*R_path(:,2,i), 'color', diagrams.colors.green);
+    diagrams.arrow(p_path(:,i), p_path(:,i)+UNIT_SIZE*R_path(:,3,i), 'color', diagrams.colors.blue);
+end
+
+diagrams.dot(p_path(:,1), 'color', diagrams.colors.green);
+diagrams.dot(p_path(:,end), 'color', diagrams.colors.red);
+
+diagrams.redraw(); hold off
