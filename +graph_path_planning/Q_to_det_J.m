8461644c288f320349923873395bf18b98376826
Alex Elias <aelias36@yahoo.com>
2024-12-06 18:16:00 -0500

    Create Q_to_det_J.m

---
+++ +graph_path_planning/Q_to_det_J.m
@@ -0,0 +1,14 @@
+function det_J_path = Q_to_det_J(Q_path, kin)
+% Find Jacobian determinant for each q in Q_path
+sz = size(Q_path);
+N = sz(3);
+
+det_J_path = NaN(width(Q_path), N);
+for i = 1:N
+    for j = 1:width(Q_path)
+        J = robotjacobian(kin, Q_path(:, j, i));
+        det_J_path(j, i) = det(J);
+    end
+end
+
+end
