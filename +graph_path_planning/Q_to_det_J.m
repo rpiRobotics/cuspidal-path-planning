77e12a418c4dc96fd1701a996744f2632e862c8b
Alex Elias <aelias36@yahoo.com>
2024-12-09 16:24:07 -0500

    Update Q_to_det_J.m

--- +graph_path_planning/Q_to_det_J.m
+++ +graph_path_planning/Q_to_det_J.m
@@ -7,6 +7,9 @@ det_J_path = NaN(width(Q_path), N);
 for i = 1:N
     for j = 1:width(Q_path)
         J = robotjacobian(kin, Q_path(:, j, i));
+        if width(J)==3 % spatial manipulators
+            J = J(4:6, :); % Ignore orientation part
+        end
         det_J_path(j, i) = det(J);
     end
 end
